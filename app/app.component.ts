f7bd92e184383078e1a3454b9f2df48cb43d525f
Liz Bennett <liz@bennettworld.org>
2016-07-08 13:53:33 -0700

    Basic layout finished with toolbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MdIcon,MdIconRegistry } from '@angular2-material/icon';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
+  directives: [MdToolbar, MD_BUTTON_DIRECTIVES, MdIcon],
+  providers: [MdIconRegistry]
 })
 export class AppComponent { }
