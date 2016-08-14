b108d5e825061d3e4d9377489f7d5719918bc3b8
Liz Bennett <liz@bennettworld.org>
2016-08-14 13:26:36 -0700

    Refactor main classes to align with current quickstart

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,16 @@ import { MdToolbar } from '@angular2-material/toolbar';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MdIcon,MdIconRegistry } from '@angular2-material/icon';
 import {ContentComponent} from "./content/content.component";
+import {appRouterProviders} from "./app.routes";
+import {MarkDownRenderer} from "./services/markdown-renderer.service";
+import {PostsService} from "./services/posts.service";
+import {HTTP_PROVIDERS} from "@angular/http";
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css'],
   directives: [MdToolbar, MD_BUTTON_DIRECTIVES, MdIcon, ContentComponent],
-  providers: [MdIconRegistry]
+  providers: [HTTP_PROVIDERS, PostsService, MarkDownRenderer, appRouterProviders, MdIconRegistry]
 })
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,4 @@
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {HTTP_PROVIDERS} from "@angular/http";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-import {AppComponent} from './app.component';
-import {PostsService} from "./services/posts.service";
-import {MarkDownRenderer} from "./services/markdown-renderer.service";
-import {appRouterProviders} from "./app.routes";
-
-bootstrap(AppComponent, [HTTP_PROVIDERS, PostsService, MarkDownRenderer, appRouterProviders]);
+platformBrowserDynamic().bootstrapModule(AppModule);
